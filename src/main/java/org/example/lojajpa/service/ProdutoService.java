8156c681dfccd05f4f6064f5b6b58d9662d99f59
joao_zamparetti <joao_zamparetti@estudante.sesisenai.org.br>
2026-03-11 21:17:43 -0300

    Produto repositório

---
+++ src/main/java/org/example/lojajpa/service/ProdutoService.java
@@ -0,0 +1,12 @@
+package org.example.lojajpa.service;
+
+import org.example.lojajpa.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+
+
+}
