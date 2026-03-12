3bde88ee396560e56d1470731be8d9fe8b8daf4e
joao_zamparetti <joao.ds.zantti@gmail.com>
2026-03-11 21:29:49 -0300

    ProdutoService métodos

--- src/main/java/org/example/lojajpa/service/ProdutoService.java
+++ src/main/java/org/example/lojajpa/service/ProdutoService.java
@@ -1,12 +1,29 @@
 package org.example.lojajpa.service;
 
+import org.example.lojajpa.model.Produto;
 import org.example.lojajpa.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProdutoService {
 
     private final ProdutoRepository repository;
 
+    public ProdutoService(ProdutoRepository repository){
+        this.repository = repository;
+    }
 
+    public Produto salvar(Produto produto){
+        return repository.save(produto);
+    }
+
+    public Produto buscar(Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Produto> listar(){
+        return repository.findAll();
+    }
 }
